1fa96b288e23e9ea308f6d3c274e88dd91615535
Timothe Clement <timothe.clement@ynov.com>
2026-09-22 15:10:36 +0200

    chore: Suppression du dossier backend et des tout ce qu'il contient. Suppression du package-lock.json.

--- dev/backend/src/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-Flight::route('GET /geocaptchas', function() {
-    echo json_encode(["message" => "Liste des GeoCaptchas"]);
-});
-
-Flight::route('POST /geocaptchas', function() {
-    $data = Flight::request()->data;
-    echo json_encode(["message" => "GeoCaptcha créé", "data" => $data]);
-});
