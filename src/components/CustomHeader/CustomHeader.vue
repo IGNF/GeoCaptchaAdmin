4b53663a044eda26910d0ad74e8038e857105d0a
Timothe Clement <timothe.clement@ynov.com>
2026-09-24 11:40:02 +0200

    refactor: (WIP) Creation of a new header conserving the Dsfr header specs while allowing more customisation

---
+++ src/components/CustomHeader/CustomHeader.vue
@@ -0,0 +1,14 @@
+<script type="ts" setup>
+  import type { CustomHeaderProps } from "@/components/CustomHeader/CustomHeader.types";
+  import type { DsfrLanguageSelectorElement } from "@gouvminint/vue-dsfr";
+
+  import { FocusTrap } from "focus-trap-vue";
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
